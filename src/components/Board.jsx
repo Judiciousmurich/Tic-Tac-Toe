d3b63a99c5e19b273e39311d3990c9036394e59a
judiciousmurich <murichjudicious45@gmail.com>
2023-10-09 21:41:43 +0300

    feat:add styles

---
+++ src/components/Board.jsx
@@ -0,0 +1,24 @@
+
+
+function Board({ xIsNext, squares, onPlay }) {
+    function handleClick(i) {
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      const nextSquares = squares.slice();
+      if (xIsNext) {
+        nextSquares[i] = 'X';
+      } else {
+        nextSquares[i] = 'O';
+      }
+      onPlay(nextSquares);
+    }
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+      status = 'Winner: 
+      ' + winner;
+    } else {
+      status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+}
