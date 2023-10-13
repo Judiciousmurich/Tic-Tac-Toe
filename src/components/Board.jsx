b77c149d53ea487dc48f51d64fd50abbed03c51a
judiciousmurich <murichjudicious45@gmail.com>
2023-10-13 22:42:58 +0300

    feat:add styles

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,11 +1,17 @@
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
 
-  return (
-    <>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={handleClick} />
-        //...
-        </>
-  );
+
+function Board({ xIsNext, squares, onPlay }) {
+    function handleClick(i) {
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      const nextSquares = squares.slice();
+      if (xIsNext) {
+        nextSquares[i] = 'X';
+      } else {
+        nextSquares[i] = 'O';
+      }
+      onPlay(nextSquares);
+    }
+   
 }
