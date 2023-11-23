55165dbcbd121964b8d374e842e10b1b04bff9b6
judiciousmurich <murichjudicious45@gmail.com>
2023-11-23 23:09:12 +0300

    feat: add styles

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const TicTacToe = () => {
   return (
     <div className='text-center'>
-        <h1 className="title">TicTacToe <span>Judicious</span></h1>
+        <h1 className="mt-[8] text-white text-xl flex">TicTacToe <span>Judicious</span></h1>
         <div className="board"></div>
         <buttton className="reset">Reset</buttton>
     </div>
