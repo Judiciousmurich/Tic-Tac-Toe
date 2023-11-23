789b53ba6d647dad4766317453a94bda4ffe1663
judiciousmurich <murichjudicious45@gmail.com>
2023-11-23 23:01:47 +0300

    feat: add styles

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const TicTacToe = () => {
   return (
     <div className='container'>
-        <h1 className="title">TicTacToe</h1>
+        <h1 className="title">TicTacToe <span>Judicious</span></h1>
     </div>
   )
 }
