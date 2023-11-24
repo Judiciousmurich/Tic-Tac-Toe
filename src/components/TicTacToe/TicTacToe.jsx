0aeb71869b42ee4826ab82aac461a124154d2c3e
judiciousmurich <murichjudicious45@gmail.com>
2023-11-24 23:34:48 +0300

    feat:add styles

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -4,7 +4,9 @@ const TicTacToe = () => {
   return (
     <div className='text-center'>
         <h1 className="mt-[8] text-white text-2xl flex items-center justify-center">TicTacToe <span className="text-[#26ffcb] pl-[5]">Judicious</span></h1>
-        <div className="board"></div>
+        <div className="board">
+          <div className="row1"></div>
+        </div>
         <buttton className="w-6 h-6 border-none outline-none cursor-pointer rounded bg-[#1f3540] text-[#26ffcb text-base] mt-4 mb-8">Reset</buttton>
     </div>
   )
