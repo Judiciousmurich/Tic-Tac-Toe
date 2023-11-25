f485ee50def6557da77cb03f09cc1faf5fcf96b2
judiciousmurich <murichjudicious45@gmail.com>
2023-11-25 21:06:15 +0300

    feat:add boxes

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -1,35 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const TicTacToe = () => {
   return (
     <div className='text-center'>
-        <h1 className="mt-[8] text-white text-2xl flex items-center justify-center">TicTacToe <span className="text-[#26ffcb] pl-[5]">Judicious</span></h1>
-        <div className="board">
-          <div className="row1">
-            <div className="boxes"></div>
-            <div className="boxes"></div>
-            <div className="boxes"></div>
-          </div>
-          <div className="row2">
-            <div className="boxes"></div>
-            <div className="boxes"></div>
-            <div className="boxes"></div>
-          </div>
+      <h1 className="mt-[8] text-white text-2xl flex items-center justify-center">TicTacToe <span className="text-[#26ffcb] pl-[5]">Judicious</span></h1>
+      <div className="board">
+        <div className="row1">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
         </div>
-
+        <div className="row2">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
         <div className="row3">
-            <div className="boxes"></div>
-            <div className="boxes"></div>
-            <div className="boxes"></div>
-          </div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
         </div>
+      </div>
 
-        
-        
-        
-        <buttton className="w-6 h-6 border-none outline-none cursor-pointer rounded bg-[#1f3540] text-[#26ffcb text-base] mt-4 mb-8">Reset</buttton>
-    </div >
-  )
+      <button className="w-6 h-6 border-none outline-none cursor-pointer rounded bg-[#1f3540] text-[#26ffcb] text-base mt-4 mb-8">Reset</button>
+    </div>
+  );
 }
 
-export default TicTacToe
+export default TicTacToe;
