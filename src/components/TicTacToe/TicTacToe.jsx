79971179704217166c3c88b344b044068c578390
judiciousmurich <murichjudicious45@gmail.com>
2023-12-02 17:56:00 +0300

    feat:add styles

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -1,36 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TicTacToe = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+
+  const handleClick = (index) => {
+    if (board[index] || calculateWinner(board)) {
+      return;
+    }
+
+    const newBoard = [...board];
+    newBoard[index] = isXNext ? 'X' : 'O';
+    setBoard(newBoard);
+    setIsXNext(!isXNext);
+  };
+
+  const renderSquare = (index) => {
+    return (
+      <div className="boxes" onClick={() => handleClick(index)}>
+        {board[index]}
+      </div>
+    );
+  };
+
+  const winner = calculateWinner(board);
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${isXNext ? 'X' : 'O'}`;
+
   return (
     <div className='text-center'>
-      <h1 className="mt-8 text-white text-2xl flex items-center justify-center">TicTacToe <span className="text-[#26ffcb] pl-5">Judicious</span></h1>
+      <h1 className="mt-8 text-white text-2xl flex items-center justify-center">
+        TicTacToe <span className="text-[#26ffcb] pl-5">Judicious</span>
+      </h1>
       <div className="board">
         <div className="flex">
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-        </div>
-        <div className="flex">
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-        </div>
-        <div className="flex">
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-        </div>
-      </div>
-
-      <button className="w-6 h-6 border-none outline-none cursor-pointer rounded bg-[#1f3540] text-[#26ffcb] text-base mt-4 mb-8">Reset</button>
-    </div>
-  );
-}
-
-export default TicTacToe;
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+          {renderSquare(3)}
+          {renderSquare(4)}
+       
