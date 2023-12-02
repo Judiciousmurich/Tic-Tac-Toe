ee2174ea0de0032792dd958e05f83377f669f8fb
judiciousmurich <murichjudicious45@gmail.com>
2023-12-02 17:47:41 +0300

    feat:add styles

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -21,6 +21,7 @@ const TicTacToe = () => {
           <div className="boxes"></div>
           <div className="boxes"></div>
           <div className="boxes"></div>
+          <div className="boxes"></div>
         </div>
       </div>
 
