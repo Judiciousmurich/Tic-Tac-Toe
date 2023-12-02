292dc57d121df164a80dd1c54344a9d3026e60f8
judiciousmurich <murichjudicious45@gmail.com>
2023-12-02 17:44:08 +0300

    feat:add styles

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -3,28 +3,4 @@ import React from 'react';
 const TicTacToe = () => {
   return (
     <div className='text-center'>
-      <h1 className="mt-[8] text-white text-2xl flex items-center justify-center">TicTacToe <span className="text-[#26ffcb] pl-[5]">Judicious</span></h1>
-      <div className="board">
-        <div className="flex  w-6 h-6">
-          <div className="boxes">
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-
-
-          </div>
-        </div>
-        <div className="row2">
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-        <div className="row3">
-        </div>
-      </div>
-
-      <button className="w-6 h-6 border-none outline-none cursor-pointer rounded bg-[#1f3540] text-[#26ffcb] text-base mt-4 mb-8">Reset</button>
-    </div>
-  );
-}
-
-export default TicTacToe;
+      <h1 className="mt-8 text-white text-
