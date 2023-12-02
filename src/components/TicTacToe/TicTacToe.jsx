1a481c137af826b526b44d1757b968662e9df4e0
judiciousmurich <murichjudicious45@gmail.com>
2023-12-02 17:42:04 +0300

    feat:add styles

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -6,7 +6,10 @@ const TicTacToe = () => {
       <h1 className="mt-[8] text-white text-2xl flex items-center justify-center">TicTacToe <span className="text-[#26ffcb] pl-[5]">Judicious</span></h1>
       <div className="board">
         <div className="flex  w-6 h-6">
+          <div className="boxes">
           <div className="boxes"></div>
+
+          </div>
         </div>
         <div className="row2">
           <div className="boxes"></div>
