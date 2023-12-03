4f3aa2b2c4563f572105f60447eaf5d2e94d5ed1
judiciousmurich <murichjudicious45@gmail.com>
2023-12-03 17:46:39 +0300

    feat:add styles

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -31,7 +31,7 @@ const TicTacToe = () => {
   return (
     <div className='text-center'>
       <h1 className="mt-8 text-white text-2xl flex items-center justify-center">
-        TicTacToe <span className="text-[#26ffcb] pl-5">Judicious</span>
+        TicTacToe <span className="text-[#26ffcb] pl-5">judy</span>
       </h1>
       <div className="board">
         <div className="flex">
